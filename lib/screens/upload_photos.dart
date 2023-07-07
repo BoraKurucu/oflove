7ade838e9995a87c0fd6d3227d5811db32bcfbaa
Bora Kurucu <borakurucu11@gmail.com>
2023-07-07 20:25:43 +0300

    Can remove uploaded photos in photo upload screen

--- lib/screens/upload_photos.dart
+++ lib/screens/upload_photos.dart
@@ -9,7 +9,7 @@ class UploadPhotosScreen extends StatefulWidget {
 }
 
 class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
-  List<PlatformFile>? selectedImages;
+  List<PlatformFile> selectedImages = [];
   int currentIndex = 0;
 
   Future<void> _pickImages() async {
@@ -20,15 +20,16 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
 
     if (result != null) {
       setState(() {
-        selectedImages = result.files;
-        currentIndex = 0; // Reset current index when new images are picked
+        selectedImages.addAll(result.files);
+        currentIndex =
+            selectedImages.length - 1; // Set current index to the last image
       });
     }
   }
 
   Future<void> _uploadImages() async {
-    if (selectedImages != null) {
-      for (final image in selectedImages!) {
+    if (selectedImages.isNotEmpty) {
+      for (final image in selectedImages) {
         final Uint8List? bytes = image.bytes;
 
         if (bytes != null) {
@@ -56,13 +57,24 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
 
   void _goToPreviousPhoto() {
     setState(() {
-      currentIndex = (currentIndex - 1).clamp(0, selectedImages!.length - 1);
+      currentIndex = (currentIndex - 1).clamp(0, selectedImages.length - 1);
     });
   }
 
   void _goToNextPhoto() {
     setState(() {
-      currentIndex = (currentIndex + 1).clamp(0, selectedImages!.length - 1);
+      currentIndex = (currentIndex + 1).clamp(0, selectedImages.length - 1);
+    });
+  }
+
+  void _removeSelectedPhoto() {
+    setState(() {
+      selectedImages.removeAt(currentIndex);
+      if (selectedImages.isEmpty) {
+        currentIndex = 0; // Set current index to 0 if all images are removed
+      } else if (currentIndex >= selectedImages.length) {
+        currentIndex--; // Adjust current index if the last image is removed
+      }
     });
   }
 
@@ -93,20 +105,25 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    'Great Choice! Upload your photos to shine like a true star!',
+                    selectedImages.isNotEmpty
+                        ? 'Great Choice! Add more photos to shine like a true star!'
+                        : 'Great Choice! Upload your photos to shine like a true star!',
                     style: TextStyle(
                       fontSize: 28,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
                     ),
+                    textAlign: TextAlign.center,
                   ),
                   SizedBox(height: 20),
                   ElevatedButton(
                     onPressed: _pickImages,
-                    child: Text('Select Photos'),
+                    child: Text(selectedImages.isNotEmpty
+                        ? 'Add More Photos'
+                        : 'Select Photos'),
                   ),
                   SizedBox(height: 20),
-                  if (selectedImages != null && selectedImages!.isNotEmpty)
+                  if (selectedImages.isNotEmpty)
                     Column(
                       children: [
                         Text(
@@ -120,9 +137,23 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
                           alignment: Alignment.center,
                           children: [
                             Image.memory(
-                              selectedImages![currentIndex].bytes!,
+                              selectedImages[currentIndex].bytes!,
                               height: 400,
                               width: 400,
+                            ),
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: Opacity(
+                                opacity: 1.0,
+                                child: IconButton(
+                                  onPressed: _removeSelectedPhoto,
+                                  icon: Icon(
+                                    Icons.delete,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
                             ),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -145,10 +176,8 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
                           height: 60,
                           width: double.infinity,
                           child: ElevatedButton(
-                            onPressed: () {
-                              _uploadImages();
-                            },
-                            child: Text('Upload All Selected Photos'),
+                            onPressed: _uploadImages,
+                            child: Text('Finish and Upload Photos'),
                           ),
                         ),
                       ],
