980f0bb0004045b09b5b41910486ef9eebbc359d
ub <ubilgin99@gmail.com>
2023-07-16 13:37:25 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -50,12 +50,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
 
-      home: BirthdayScreen(
-        uid: "test_id6",
-        email: "test@mail.com",
-      ),
+      // home: BirthdayScreen(
+      //   uid: "test_id6",
+      //   email: "test@mail.com",
+      // ),
 
-      //home: WelcomePage(uid: "test_id3"),
+      home: WelcomePage(uid: "test_id3"),
     );
   }
 }
